70fec41ba1d620520d7ac9b4d77325addd6ca3ea
arpangreat <arpanthegreat41@gmail.com>
2022-10-19 00:44:17 +0530

    clean ups

---
+++ C/arrays.c
@@ -0,0 +1,14 @@
+/*
+ * An Array is a collection of some data types at contiguous memory locations
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  int arr[7] = {1, 2, 3, 4, 5, 6};
+
+  printf("%d", arr[6]);
+  return 0;
+}
---
+++ C/dynamic_array.c
@@ -0,0 +1,15 @@
+/*
+ *
+ * when we try to create an array with dynamic values
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // dataType arrayName[number of values] = {values} - Static Array / 1D
+
+  int *arr;
+  int n, i;
+  return 0;
+}
